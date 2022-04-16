331a9ebe2d565d1549ae78b296fdd241dab9cf3b
pralhad88 <pralhad18@navgurukul.org>
2022-04-16 23:39:14 +0530

    model data type defined

--- src/model/user/index.js
+++ src/model/user/index.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: "string",
-    email: "string",
-    password: "string",
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    userName: { type: String, unique: true, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     createDate: "date",
     updatedDate: "date",
   },
